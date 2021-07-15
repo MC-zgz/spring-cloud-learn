a2f5ed407aed611e1eea46479a952602e7b05a39
APJ\zhangguozhuang <williezhang@apjcorp.com>
2021-07-15 18:00:17 +0800

    payment module over

---
+++ spring-cloud/cloud-provider-payment8001/src/main/java/com/zgz/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,26 @@
+package com.zgz.springcloud.service.impl;
+
+import com.zgz.springcloud.dao.PaymentDao;
+import com.zgz.springcloud.entities.Payment;
+import com.zgz.springcloud.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    @Resource
+    private PaymentDao paymentDao;
+
+    @Override
+    public int create(Payment payment) {
+        return paymentDao.create(payment);
+    }
+
+    @Override
+    public Payment getPaymentById(Long id) {
+        return paymentDao.getPaymentById(id);
+    }
+}
