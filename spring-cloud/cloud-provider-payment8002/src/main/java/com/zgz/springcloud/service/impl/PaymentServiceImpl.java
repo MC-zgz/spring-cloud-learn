ae0034c1b5c6a4d2516309df895aaff572ecef8c
APJ\zhangguozhuang <williezhang@apjcorp.com>
2021-07-16 17:46:27 +0800

    22-支付微服务集群over

---
+++ spring-cloud/cloud-provider-payment8002/src/main/java/com/zgz/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,25 @@
+package com.zgz.springcloud.service.impl;
+
+import com.zgz.springcloud.dao.PaymentDao;
+import com.zgz.springcloud.entities.Payment;
+import com.zgz.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    @Resource
+    private PaymentDao paymentDao;
+
+    @Override
+    public int create(Payment payment) {
+        return paymentDao.create(payment);
+    }
+
+    @Override
+    public Payment getPaymentById(Long id) {
+        return paymentDao.getPaymentById(id);
+    }
+}
