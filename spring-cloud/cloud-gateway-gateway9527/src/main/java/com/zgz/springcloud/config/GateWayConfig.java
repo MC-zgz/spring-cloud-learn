ec6760fbcd69299d3b03d2fa647ae8700fdaedf5
LAPTOP-DMAV40QO\ASUS <2609119959@qq.com>
2021-07-25 23:21:37 +0800

    config no over

--- spring-cloud/cloud-gateway-gateway9527/src/main/java/com/zgz/springcloud/config/GateWayConfig.java
+++ spring-cloud/cloud-gateway-gateway9527/src/main/java/com/zgz/springcloud/config/GateWayConfig.java
@@ -1,4 +1,4 @@
-package config;
+package com.zgz.springcloud.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
